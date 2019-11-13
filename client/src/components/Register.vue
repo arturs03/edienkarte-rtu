e8d74e9b3364986ef7a9b99a9a2202cafb2e8b6d
Arturs Tkacenko <tkacenkoarturs@gmail.com>
2019-11-13 22:17:26 +0200

    added basics of register module

---
+++ client/src/components/Register.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email"/>
+    <br/>
+    <input type="password" name="password" placeholder="password" v-model="password"/>
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticationService from '../services/authenticationService'
+
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+
+  watch: {
+    email (value) {
+      console.log(value)
+    }
+  },
+
+  methods: {
+    async register () {
+      const response = await authenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+
+      console.log(response.data)
+    }
+  }
+}
+</script>
--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-console.log('VISS BUMBAS')
+console.log('Lets go champ')
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -10,9 +10,9 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hellow world!'
+    message: `Hello ${req.body.email}! Your user was registered, lets go champ`
   })
 })
 
