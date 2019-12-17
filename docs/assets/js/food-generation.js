6659df1e328638aaa3e4afde28254b028a84b9ac
pe4ons <57841981+pe4ons@users.noreply.github.com>
2019-12-17 23:11:36 +0200

    Receptes pievienošanas
    teksti un receptes

--- docs/assets/js/food-generation.js
+++ docs/assets/js/food-generation.js
@@ -2,7 +2,7 @@ var recepies =
     [
         {
             "recepieTitle": "Omlete ar tostermaizi un sulu",
-            "recepieDescription": "3 olas, piens 100ml, 2 tosermaizes + sviests, sula pēc jūsu izvēles",
+            "recepieDescription": "3 olas, piens 100ml, 2 tostermaizes + sviests, sula pēc jūsu izvēles",
             "recepieBenefits": {
                 "gains": 90,
                 "vitamins": 0,
@@ -13,7 +13,7 @@ var recepies =
         },
         {
             "recepieTitle": "Saldskābā vistas fileja ar rīsiem",
-            "recepieDescription": "200 g vistas filejas, 2 ēd. k. sojas mērces, 1 ēd. k. kartupeļu cietes, saldskābā mērce, pēc izvēles 1 paciņas rīsu",
+            "recepieDescription": "200 g vistas filejas, 2 ēd. k. sojas mērces, 1 ēd. k. kartupeļu cietes, saldskābā mērce, pēc izvēles 1 paciņa rīsu",
             "recepieBenefits": {
                 "gains": 70,
                 "vitamins": 20,
@@ -24,16 +24,50 @@ var recepies =
         },
         {
             "recepieTitle": "Vistas karbonāde ar griķiem un zaļumiem",
-            "recepieDescription": "200 g vistas filejas, 1 olas, 2 ēd. k. miltu, krējums, pēc izvēles 1 paciņas griķu, salāt lapas ar tomātiem un gurķiem",
+            "recepieDescription": "200 g vistas filejas, 1 olas, 2 ēd. k. miltu, krējums, pēc izvēles 1 paciņa griķu, salātlapas ar tomātiem un gurķiem",
             "recepieBenefits": {
                 "gains": 70,
                 "vitamins": 0,
                 "fats": 30
             },
             "recepieImage": "http://media.gardedis.lv/cache/eb/f6/ebf6b337d75256431b4f76d4061177d4.jpg",
+            "isVegan": true,
+        },
+        {
+            "recepieTitle": "Biezpiena plācenīši",
+            "recepieDescription": "biezpiens 500g, 2 olas, kviešu milti, 6 ēd.k. miltu, 2 ēd.k. cukura,5 ēd.k. saulespuķu eļļas",
+            "recepieBenefits": {
+                "gains": 85,
+                "vitamins": 5,
+                "fats": 10
+            },
+            "recepieImage": "http://www.brown-sugar.lv/images/products/kulinarija/pankukas/biezpiena-placenisi-ar-ievarijumu-un-krejumu-2-gab/th/700x700_6/biezpienaplacenisi1.jpg",
             "isVegan": false,
         },
+        {
+            "recepieTitle": "Franču siera zupa ar vistu",
+            "recepieDescription": "vistas fileja 500g, kausētais siers 200g, kartupeļi 400g, sīpoli 150g, burkāni 180g, sviests pēc garšas, sāls pēc garšas, maltie pipari pēc garšas, zaļumi pēc garšas, lauru lapa 3gab, sausiņi 100g",
+            "recepieBenefits": {
+                "gains": 70,
+                "vitamins": 0,
+                "fats": 30
+            },
+            "recepieImage": "http://media.gardedis.lv/cache/b0/ab/b0ab7eacb24001015dc120a180e2f342.jpg",
+            "isVegan": false,
+        },
+        {
+            "recepieTitle": "Klasiskie grieķu salāti",
+            "recepieDescription": "3 ēd.k. olīveļļa, 1,5 ēd.k. citronu sula, ķiploki 1 daiviņa, 1/2 tēj.k. sausie oregano, 1/4 tēj.k. jūras sāls, 1/4 tēj.k. maltie pipari, tomāti 3gab, sarkanie sīpoli 1gab, gurķi 2gab, paprika 2gab, siers feta 120g ,olīvas 150g",
+            "recepieBenefits": {
+                "gains": 90,
+                "vitamins": 0,
+                "fats": 10
+            },
+            "recepieImage": "http://www.dzivei.lv/wp-content/uploads/2018/06/grieku_salati.jpg",
+            "isVegan": true,
+        }
     ];
+
 
 function generateRecepie() {
     let dati = [];
@@ -54,7 +88,11 @@ function generateRecepie() {
 
     var recepieNode = '';
 
-    console.log(recepies[0].recepieBenefits);
+
+    for(var x = 0; x < recepies.length; x++){
+        console.log(recepies[x].recepieBenefits);
+    }
+
     for (let i = 0; i < recepies.length; i++) {
         let id = recepies[i].recepieTitle.replace(/[^a-zA-Z ]/g, "").replace(/ /g,"-");
         recepieNode += `
