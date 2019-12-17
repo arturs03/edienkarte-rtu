d8d90863b2b75697edd2da0671f7a7bdf3798686
arturs03 <tkacenkoarturs@gmail.com>
2019-12-17 23:11:04 +0200

    removed unnnecessary files

--- client/src/services/authenticationService.js
+++
@@ -1,7 +0,0 @@
-import api from '@/services/api'
-
-export default {
-  register (credentials) {
-    return api().post('register', credentials)
-  }
-}
--- server/src/routes.js
+++
@@ -1,8 +0,0 @@
-const AuthenticationController = require('./controllers/AuthenticationController')
-const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
-
-module.exports = (app) => {
-  app.post('/register',
-    AuthenticationControllerPolicy.register,
-    AuthenticationController.register)
-}
